3532b4135db26cac58a00f50c90fafaf638babf3
Vampire_5 <mhshifat757@gmail.com>
2018-09-30 00:02:12 +0600

    icpc selection contest (team work)

---
+++ Contest/H - The Great Ball.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool myfunction(int a,int b)
+{
+    return a>b;
+}
+int main()
+{
+    long int t,n,a,b,i,j,p;
+    vector <long int> v1;
+    vector <long int> v2;
+    cin>>t;
+    while(t>0)
+    {
+        p=0;
+        cin>>n;
+        for(i=0;i<n;i++)
+        {
+            cin>>a;
+            v1.push_back(a);
+            cin>>b;
+            v2.push_back(b);
+        }
+        sort(v1.begin(),v1.end(),myfunction);
+        sort(v2.begin(),v2.end());
+
+  ///      for(i=0,j=0;i<n,j<n;i++,j++)
+  //      {
+    //        cout<<v1[i]<<"\t"<<v2[j]<<endl;
+      //  }
+
+        for(i=0;i<n;i++)
+        {
+            if(v1[i]>=v2[i])
+                p++;
+        }
+        cout<<n-p<<endl;
+        v1.clear();
+        v2.clear();
+        t--;
+
+    }
+    return 0;
+}
